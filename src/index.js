570e0e1a6754738f0599bc83a03baed23c3ad8e6
Juan Sanchez <juansan1993@hotmail.com>
2023-04-12 13:11:30 -0500

    Add fetch request and take id of the game created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './index.scss';
 
 console.log('Hello World!');
+
+const options = {
+  method: 'POST',
+  body: JSON.stringify({ name: 'Mario bross' }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+};
+
+fetch(
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+  options
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // Extract the game ID from the response
+    const gameID = data.result.split(': ')[1].slice(0, -7);
+    console.log(data);
+    console.log(gameID);
+  });
